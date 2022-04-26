e6bcce3cf466418f4dbb3808fd42fab6a24a630c
Fish <yxzjerryfish@hotmail.com>
2022-04-26 21:51:45 +0800

    删掉之前工程测试代码

--- src/main/java/com/example/jpademo/jpaDemo/controller/HelloController.java
+++ src/main/java/com/example/jpademo/jpaDemo/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.example.jpademo.jpaDemo.controller;
 
 
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Autowired
-    EntityManager entityManager;
-
     @GetMapping("/1")
     @Transactional(rollbackFor = Exception.class)
-    public String sayHello() throws InterruptedException {
-        System.out.println(Thread.currentThread().getName() + entityManager.toString());
-        Thread.sleep(60*1000L);
+    public String sayHello() {
         return "Hello Spring!";
     }
 }
