84d31ccc8a08be0e3417ae15d90a5b97cbf4702d
Fish <yxzjerryfish@hotmail.com>
2022-04-27 22:22:25 +0800

    优化一些代码警告，发现需要一个文件导入框架，先去写一个文件导入的框架

--- src/main/java/com/example/jpademo/jpaDemo/JpaDemoApplication.java
+++ src/main/java/com/example/jpademo/jpaDemo/JpaDemoApplication.java
@@ -20,7 +20,7 @@ public class JpaDemoApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		initService.initUserService();
 	}
 }
